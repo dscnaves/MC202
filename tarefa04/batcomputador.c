7016aa9849873e69eb9a3636435a8105756e65f7
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-09-13 20:35:31 -0300

    Inicio da resolução tarefa4

---
+++ tarefa04/batcomputador.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MIN_VETOR 8 //Tamanho mínimo do vetor
+
+typedef struct {
+    int * dados;
+    int alocado;
+    int utilizados;
+} Vector;
+
+typedef Vector * p_vector;
+
+void bat_alloc(int N){ //N é o tamanho do vetor
+    p_vector v;
+    v = malloc(sizeof(Vector));
+
+    if (N >= MIN_VETOR) {        
+        v->dados = malloc(2*N*sizeof(int));
+        v->alocado = 2*N;
+        v->utilizados = 0;
+    }
+
+    else {
+        v->dados = malloc(MIN_VETOR*sizeof(int));
+        v->alocado = MIN_VETOR;
+        v->utilizados = 0;
+    }
+
+    for (int i = 0; i<N; i++){
+        scanf("%d", &v->dados[i]);
+        v->utilizados++;
+    }
+
+    printf("%p", &v->dados[0]);
+}
+
+int main(){
+
+    int n; //Número de operações a serem realizadas
+    scanf("%d", &n);
+    
+    char command[10];
+    for (int x = 0; x<n; x++){
+        scanf("%s", command);
+
+        if (strcmp(command,"bat-alloc") == 0){
+            int N; //Qtde de números alocados
+            scanf("%d", &N);
+            bat_alloc(N);
+        }
+
+        else if (strcmp(command,"bat-free") == 0){
+            p_vector v; //Endereço de memória que se quer liberar
+            scanf("%p", v);
+
+            free(v->dados);
+            free(v);
+        }
+
+        else if (strcmp(command,"bat-print") == 0){
+            p_vector v; //Endereço de memória que se quer imprimir
+            scanf("%p", v);
+
+            for (int i = 0; i < v->utilizados; i++){
+                printf("%d ", v->dados[i]);
+            }
+        }
+
+        else if (strcmp(command,"bat-uso") == 0){
+
+        }
+
+    }
+
+    return 0;
+}
