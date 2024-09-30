f172dd72e36430ba18bb62852b257849f0dced70
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-09-30 14:39:07 -0300

    Correção função liberar jogo e leitura dos dados iniciais

--- tarefa06/cobra.c
+++ tarefa06/cobra.c
@@ -45,6 +45,7 @@ void imprimir(int m, int n, char ** matriz){
         }
         printf("\n");
     }
+    printf("\n");
 }
 
 void movimenta_cobra(char ** matriz, No * cabeca, No * cauda, Posicao * prox_pos){
@@ -67,28 +68,53 @@ void movimenta_cobra(char ** matriz, No * cabeca, No * cauda, Posicao * prox_pos
     matriz[cabeca->x][cabeca->y] = '#'; // Coloca a nova cabeça
 }
 
-int verificar_colisao(No * cabeca, int x, int y){
-    
+int verificar_colisao(char ** matriz, int x, int y){
+    if (matriz[x][y] == '#'){
+        printf("GAME OVER\n");
+        return 1;
+    }
+    return 0;
+}
+
+void liberar_jogo(char ** matriz, No * cauda, int m){
+    for (int i = 0; i < m; i++){
+        free(matriz[i]);
+    }
+    free(matriz);
+
+    No * p_atual = cauda;
+    while (p_atual != NULL){
+        No * prox = p_atual->prox;
+        free(p_atual);
+        p_atual = prox;
+    }
+
 }
 
 int main(){
     int m, n; //Dimensões da matriz
-    scanf("MATRIZ %d %d", &m, &n);
+    char command[7];
+    scanf("%s",command);
+    scanf("%d %d", &m, &n);
     
     //Alocando a matriz
     char ** matriz = alocar_matriz(m,n);
 
     //Atribuindo '_' a cada posição
     inicializar_matriz(m,n,matriz);
-    imprimir(m,n,matriz);
 
     Posicao inicial_pos;
-    scanf("COBRA %d %d", &inicial_pos.x, &inicial_pos.y);
+    scanf("%s",command);
+    scanf("%d %d", &inicial_pos.x, &inicial_pos.y);
 
     //Inicializando a cobra
     No  * cabeca;
-    alocar_no(&cabeca,inicial_pos.x,inicial_pos.y);  
+    alocar_no(&cabeca,inicial_pos.x,inicial_pos.y);
+    //No início pos cauda == cabeca  
     No * cauda;
+    alocar_no(&cauda,cabeca->x,cabeca->y);
+    cauda->prox = cabeca;
+    cabeca->ant = cauda;
     
     
     //Atualizar matriz
@@ -101,14 +127,13 @@ int main(){
     //Jogo    
     while(1){
         //Lendo comando
-        char command[7];
-        scanf("%s", command);
+        scanf("%6s", command);
 
         //Se o comando for fruta
         if (strcmp(command,"FRUTA") == 0) {
-            Posicao * fruta;
-            scanf("%d %d", &fruta->x, &fruta->y);
-            matriz[fruta->x][fruta->y] = '*'; //Inserir fruta
+            Posicao fruta;
+            scanf("%d %d", &fruta.x, &fruta.y);
+            matriz[fruta.x][fruta.y] = '*'; //Inserir fruta
             imprimir(m,n,matriz);
         }
 
@@ -119,25 +144,28 @@ int main(){
             prox_pos.y = cabeca->y;
 
             //Para cima
-            if (command =='w') {
+            if (strcmp(command, "w") == 0) {
                 prox_pos.x = (cabeca->x - 1 + m) % m;
             }
             //Para baixo
-            else if (command == 's'){
+            else if (strcmp(command, "s") == 0){
                 prox_pos.x = (cabeca->x + 1) % m;
             }
             //Para direita
-            else if (command == 'd'){
+            else if (strcmp(command, "d") == 0){
                 prox_pos.y = (cabeca->y + 1) % n;
             }
             //Para esquerda
-            else if (command == 'a'){
-                prox_pos.y = (cabeca->y - 1) % n;
+            else if (strcmp(command, "a") == 0){
+                prox_pos.y = (cabeca->y - 1 + n) % n;
             }
-        }
-
+        
             //Verificar colisão com o próprio corpo
-
+            int colide = verificar_colisao(matriz, prox_pos.x, prox_pos.y);
+            if (colide == 1){
+                liberar_jogo(matriz, cauda,m);
+                break;
+            }
 
             //Se a cobra encontrar uma fruta
             if (matriz[prox_pos.x][prox_pos.y] == '*') {
@@ -154,16 +182,18 @@ int main(){
 
                 if (tamanho_cobra == total_posicoes){
                     imprimir(m,n,matriz);
-                    prinft(" YOU WIN\n");
+                    printf("YOU WIN\n");
+                    liberar_jogo(matriz, cauda,m);
                     break;
                 }
             }
 
             //Se a cobra encontrar um espaço '_'
             else {
-                movimenta_cobra(matriz, cabeca, cauda, prox_pos);
+                movimenta_cobra(matriz, cabeca, cauda, &prox_pos);
             }
             imprimir(m,n,matriz);
+        }
     }
 
     return 0;
