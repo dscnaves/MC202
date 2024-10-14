66001227737dcf563a3f20132d3168a68fc4c498
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-10-14 18:40:42 -0300

    Problema de free no simplificar_negação

--- tarefa07/otimizar.c
+++ tarefa07/otimizar.c
@@ -17,7 +17,7 @@ Node* create_node(char valor, Node* left, Node* right) {
     new_node->valor = valor;
     return new_node;
 }
-
+void Liberar(Node* raiz);
 // Função para verificar se um caractere é uma variável (letra minúscula)
 int is_lower(char c) {
     return (c >= 'a' && c <= 'z');
@@ -53,22 +53,28 @@ Node* simplificar_negacao(Node* raiz) {
     if (raiz->valor == '!') {
         Node* son = raiz->left;
 
-        if (son->valor == '|') {
+        if (son->valor == '|' || son->valor == '&') {
             Node* not_left = create_node('!', simplificar_negacao(son->left), NULL);
             Node* not_right = create_node('!', simplificar_negacao(son->right), NULL);
-            raiz = create_node('&', not_left, not_right);
-        } else if (son->valor == '&') {
-            Node* not_left = create_node('!', simplificar_negacao(son->left), NULL);
-            Node* not_right = create_node('!', simplificar_negacao(son->right), NULL);
-            raiz = create_node('|', not_left, not_right);
-        } else if (son->valor == '!') {
+            Node* aux = raiz;
+            if (son->valor == '|'){
+                raiz = create_node('&', not_left, not_right);
+            }
+            else{
+                raiz = create_node('|', not_left, not_right);
+            }
+            free(aux);
+        }
+        else if (son->valor == '!') {
+            Node* aux = raiz;
             raiz = simplificar_negacao(son->left);
+            free(aux);
         }
     }
-
     raiz->left = simplificar_negacao(raiz->left);
-    raiz->right = simplificar_negacao(raiz->right);
 
+    raiz->right = simplificar_negacao(raiz->right);
+    
     return raiz;
 }
 
@@ -156,10 +162,18 @@ void imprimir(Node* raiz) {
     }
 }
 
+
+void Liberar(Node* raiz) {
+    if (raiz) {
+        Liberar(raiz->left);
+        Liberar(raiz->right);
+        free(raiz);
+    }
+}
+
 // Função principal
 int main() {
     // Construir a árvore da expressão
-    
     Node* raiz = build_tree();
 
     // Imprimir a expressão original
@@ -181,7 +195,7 @@ int main() {
     printf("\n");
 
     // Liberar memória
-    free(raiz);
+    Liberar(raiz);
 
     return 0;
 }
