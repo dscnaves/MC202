0a473ed8b53a3e8eeb4143767baa963c7bbbf35e
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-10-13 21:12:32 -0300

    implementando print_tree e is_no_interno

--- tarefa08/cidades.c
+++ tarefa08/cidades.c
@@ -12,7 +12,7 @@ typedef struct City {
 typedef struct Quad_tree{
     int x_min, x_max, y_min, y_max;
     struct Quad_tree * children[4]; //1: 1° quadrante NE, 2: 2° quadrante NO, 3: 3° quadrante SO, 4: 4° quadrante SE
-    City city;
+    City * city;
 } Quad_tree;
 
 Quad_tree * initialize_tree(int x_min, int x_max, int y_min, int y_max){
@@ -22,16 +22,46 @@ Quad_tree * initialize_tree(int x_min, int x_max, int y_min, int y_max){
     p_root->y_min = y_min;
     p_root->y_max = y_max;
     City * p_city = malloc(sizeof(City *));
+    return p_root;
 }
 
+int is_no_interno(Quad_tree * root){
+    //Se um nó é interno então ele possui pelo menos 1 filho
+    if (root->children[0] != NULL || root->children[1] != NULL || root->children[2] != NULL || root->children[3] != NULL) return 1;
+    return 0;
+}
 
+void print_tree(Quad_tree *root, int depth){
+
+    //Caso de parada: Se o nó contém uma cidade, imprime a cidade
+    if (root->city != NULL) {
+        printf("  %*sCidade %s (%d,%d)\n", depth * 2, "", root->city->name, root->city->x, root->city->y);
+    }
+
+    //Se o nó for um nó interno 
+    else if (is_no_interno(root) == 1) {
+        printf("  %*sI\n", depth * 2, "");
+
+        for (int i = 0; i < 4; i++) {
+            if (root->children[i] != NULL) {
+                print_tree(root->children[i], depth + 1);
+            } else {
+                printf("  %*sV\n", (depth + 1) * 2, "");
+            }
+        }
+    } 
+    
+    else {
+        printf("  %*sV\n", depth * 2, "");
+    }
+}
 
 int main(){
-    int w; //lado quadrado que representa o mapa
-    scanf("%d",&w);
+    int size; //lado quadrado que representa o mapa
+    scanf("%d",&size);
 
     //Inicializando a árvore
-    initialize_tree(0,w,0,w);
+    Quad_tree * root = initialize_tree(0,size,0,size);
 
     char command[1];
     while (1) {
@@ -59,7 +89,8 @@ int main(){
 
         //Impressão
         else if (strcmp(command, "p") == 0) {
-            /* code */
+            printf("Árvore:\n");
+            
         }     
     }
     return 0;
