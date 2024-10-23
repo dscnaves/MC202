ef1699e2180bab0b1ec3d7fbf60bb2aec159ee21
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-10-22 23:58:35 -0300

    implementação da função para inicializa a matriz dos acessos futuros, da função que libera memória dessa matriz e da função que constroi essa matriz

--- tarefa09/cache.c
+++ tarefa09/cache.c
@@ -1,24 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_OBJECTS 100000
+
 // Função para alocar e inicializar a estrutura de próximos acessos
-int* inicializar_prox_acesso(int num_objects, int length) {
-    int *prox_acesso = (int *)malloc(num_objects * sizeof(int));
+int* next_access_initialize(int num_objects, int length) {
+    //O array chamado next_access é usado para para armazenar quando cada objeto será acessado novamente
+
+    int *next_access = malloc(num_objects * sizeof(int *));
+    
     for (int i = 0; i < num_objects; i++) {
-        prox_acesso[i] = length; // Inicializa como não acessado mais
+        next_access[i] = length; // Inicializa como não acessado mais => lenght é um marcador cujo acesso inváido ao vetor é usado para revelar que um objeto não será mais acessado 
     }
-    return prox_acesso;
+    return next_access;
+}
+
+int** future_access_initialize(int num_objects, int length) {
+    //A matriz chamada future_access é aquela onde cada linha corresponde a um objeto, e cada linha armazena as posições na sequência em que esse objeto será acessado no futuro
+
+    int **future_access = malloc(num_objects * sizeof(int **));
+    
+    for (int i = 0; i < num_objects; i++) {
+        future_access[i] = malloc(length * sizeof(int *));
+    }
+    return future_access;
+}
+
+void future_access_free(int **future_access, int num_objects) {
+    for (int i = 0; i < num_objects; i++) {
+        free(future_access[i]);
+    }
+    free(future_access);
+}
+
+void calcular_proximos_acessos(int sequence[], int length, int **future_access, int *contador_acessos, int num_objects) {
+    //Essa função preenche a matriz future_acess
+
+    //Percorrer a lista de acesso de trás para frente para garantirmos que a matriz seja preenchida  com as posições mais próximas no futuro para cada objeto
+    for (int i = length - 1; i >= 0; i--) {
+        int obj = sequence[i];
+        
+        future_access[obj][contador_acessos[obj]] = i;
+        //Atualizando a variável que quanta a quantidade de acessos que um objeto teve
+        contador_acessos[obj]++;
+    }
 }
 
 
+
 // Função de simulação de cache
-int cache_simulation(int cache_size, int num_objects, int sequence[], int length) {
+int cache(int cache_size, int num_objects, int sequence[], int length) {
     int insercoes = 0;
 
     int *cache = (int *)malloc(cache_size * sizeof(int));
     int cache_count = 0;
 
-    int *prox_acesso = inicializar_prox_acesso(num_objects, length);
+    int *next_access = next_access_initialize(num_objects, length);
 
 
  
