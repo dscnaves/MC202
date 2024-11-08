b0cf0f9087f1bde57eb24dd64f2a1db5adb4014b
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-11-08 11:39:58 -0300

    implementando função para construir a matriz de adjcência + imprimi la + liberál la

--- tarefa11/piscapisca.c
+++ tarefa11/piscapisca.c
@@ -27,18 +27,64 @@ Graph * create_matrix(int num_vertices){
     return graph;
 }
 
-void initialize_matrix(Graph * graph, int num_vertices){
-    //Inicializa a matriz de adjacência com zeros
-    for (int i = 0; i<num_vertices; i++){
-        for (int j = 0; j<num_vertices; j++){
-            graph->adjacency_matrix[i] = 0;
+//Initiazlizing the matrix with zerous
+void initialize_matrix(Graph * graph){
+    for (int i = 0; i<graph->num_vertices; i++){
+        for (int j = 0; j<graph->num_vertices; j++){
+            graph->adjacency_matrix[i][j] = 0;
         }        
     }
     return;
 }
 
+//Building the matrix with the right conections
+void build_matrix(Graph * graph, int num_conections){
+    int x,y;
+    for (int i = 0; i<num_conections; i++){
+        scanf("%d %d",&x,&y);
+
+        graph->adjacency_matrix[x][y] = 1;
+        graph->adjacency_matrix[y][x] = 1;
+    }
+}
+
+//Printing the matriz
+void print_matrix(Graph * graph){
+    for(int i = 0; i<graph->num_vertices; i++){
+        for(int j = 0; j<graph->num_vertices; j++){
+            printf("%d ", graph->adjacency_matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return;
+}
+
+void free_matrix(Graph *graph){
+    for (int i = 0; i<graph->num_vertices; i++){
+        free(graph->adjacency_matrix[i]);
+    }
+    free(graph->adjacency_matrix);
+    free(graph);
+}
 
 int main(){
+    //Reading of data
+    int num_vertices, num_conections;
+    scanf("%d %d", &num_vertices, &num_conections);
+
+    //Creating and allocating the matrix 
+    Graph * graph = create_matrix(num_vertices);
+
+    //Initiazlizing the matrix with zerous
+    initialize_matrix(graph);
+
+    //Building the matrix with the right conections
+    build_matrix(graph, num_conections);
+
+    print_matrix(graph);
+
+    free_matrix(graph);
+    
 
     return 0;
 }
