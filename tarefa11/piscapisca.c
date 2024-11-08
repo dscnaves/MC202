69b5de316de2f655052ee6e13651703a1f019281
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-11-08 17:46:50 -0300

    ajustando a função main com as novas funções criadas

--- tarefa11/piscapisca.c
+++ tarefa11/piscapisca.c
@@ -170,8 +170,8 @@ void classifyLamps(Graph *graph, int source) {
 
 int main(){
     //Reading of data
-    int num_vertices, num_conections;
-    scanf("%d %d", &num_vertices, &num_conections);
+    int num_vertices, num_conections, source;
+    scanf("%d %d %d", &num_vertices, &num_conections, &source);
 
     //Creating and allocating the matrix 
     Graph * graph = create_matrix(num_vertices);
@@ -182,10 +182,11 @@ int main(){
     //Building the matrix with the right conections
     build_matrix(graph, num_conections);
 
-    print_matrix(graph);
-
+    //Classifica as lâmpadas e exibe o estado de cada uma
+    classifyLamps(graph, source);
+    
+    //Libera a memória alocada para o grafo
     free_matrix(graph);
     
-
     return 0;
 }
