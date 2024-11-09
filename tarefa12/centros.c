9be02a0a7283070e98212dc042bad99a346d11c1
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-11-09 10:30:06 -0300

    Configuração inicial struct

---
+++ tarefa12/centros.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+#define MAX_CITIES 100
+#define INF INT_MAX
+
+
+typedef struct {
+    char name[50];
+    int population;
+} City;
+
+
+
+int main() {
+        
+
+    return 0;
+}
