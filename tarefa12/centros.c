586b2b109befe43d21d33f5ad1b5824501de4a5b
Daniela Souza Cardoso Naves (281141) <d281141@dac.unicamp.br>
2024-11-12 22:31:14 -0300

    Implementação das função para adicionar rotas entre cidade + atualização da main

--- tarefa12/centros.c
+++ tarefa12/centros.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX_CITIES 100
+#define NOT_FOUND_city -1
 
 // Estrutura para representar uma cidade
 typedef struct {
@@ -12,7 +14,7 @@ typedef struct {
 
 // Estrutura para representar uma aresta no grafo
 typedef struct Edge {
-    int dest; // Índice da cidade de destino
+    int destination; // Índice da cidade de destino
     int weight; // Distância entre as cidades | "Peso da aresta"
     struct Edge *next; // Aponta para a próxima aresta na lista de adjacências
 } Edge;
@@ -22,6 +24,7 @@ typedef struct Edge {
 - Estrutura para representar uma lista de adjacências
 - Uma lista de adjacências para cada cidade
 Armazenando as arestas que partem desta cidade
+- Só um ponteiro que aponta para o 1° elemento da Lista de Arestas/Conexões
 */
 typedef struct AdjList {
     Edge * head;
@@ -58,9 +61,9 @@ void print_graph(Graph *graph) {
         printf("Cidade: %s (População: %d) -> ", graph->cities[i].name, graph->cities[i].population);
         
         // Percorre a lista de adjacências da cidade atual
-        Edge *edge = graph->adj_lists[i].head;
+        Edge * edge = graph->adj_lists[i].head;
         while (edge != NULL) {
-            printf(" %s (Distância: %d) ", graph->cities[edge->dest].name, edge->weight);
+            printf(" %s (Distância: %d) ", graph->cities[edge->destination].name, edge->weight);
             edge = edge->next;
             
             if (edge != NULL) {
@@ -71,11 +74,66 @@ void print_graph(Graph *graph) {
     }
 }
 
+// Adiciona uma conexão (aresta) entre duas cidades (nós) no grafo
+void add_edge(Graph * graph, int source_city, int destination_city, int weight_egde) {
+    //Criando ligação de A->B:
+    Edge * newEdge = (Edge *)malloc(sizeof(Edge));
+    
+    newEdge->destination = destination_city;
+    newEdge->weight = weight_egde;
+
+    /*Define o ponteiro next da nova Edge para apontar para a cabeça atual da lista de adjacências de source (graph->adj_lists[src].head)*/
+    newEdge->next = graph->adj_lists[source_city].head;
+
+    /*Atualiza graph->adj_lists[src].head para apontar para a nova Edge, tornando-a a nova cabeça da lista*/
+    graph->adj_lists[source_city].head = newEdge;
+
+    //Criando ligação bidirecional de B->A:
+    newEdge = (Edge *)malloc(sizeof(Edge));
+    newEdge->destination = source_city;
+    newEdge->weight = weight_egde;    
+    newEdge->next = graph->adj_lists[destination_city].head;
+    graph->adj_lists[destination_city].head = newEdge;
+}
+
+//Procura uma cidade pelo nome e retorna seu índice
+int find_city_index(Graph * graph, char * name) {
+    for (int i = 0; i < graph->num_cities; i++) {
+        if (strcmp(graph->cities[i].name, name) == 0) {
+            return i;
+        }
+    }
+    //Se não encontrar a cidade, retorna -1
+    return NOT_FOUND_city;
+}
+
 int main(){
-    int num_cities; //, num_routes;
+    int num_cities, num_routes;
     scanf("%d", &num_cities);
 
     Graph graph;
+
+    // Lendo as cidades e suas populações
+    for (int i = 0; i < num_cities; i++) {
+        scanf("%s %d", graph.cities[i].name, &graph.cities[i].population);
+    }
+
+    // Lendo o número de rotas
+    scanf("%d", &num_routes);
+    for (int i = 0; i < num_routes; i++) {
+        char city1[50], city2[50];
+        int distance;
+        scanf("%s %s %d", city1, city2, &distance);
+
+        int index1 = find_city_index(&graph, city1);
+        int index2 = find_city_index(&graph, city2);
+
+        if (index1 != NOT_FOUND_city || index2 != NOT_FOUND_city){
+            add_edge(&graph, index1, index2, distance);
+        }
+        else printf("Cidades não devidamente adicionadas");        
+    }
+    
 
     init_graph(&graph, num_cities);
 
